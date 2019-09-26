c64a36f389b41198c45a992c5a7d4f00ad76d097
EEEEEBanini <kidbuu379@hotmail.com>
2019-09-26 07:31:40 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 /* globals window */
 
-import CernerSmartEmbeddableLib from './cerner-smart-embeddable-lib';
+// import CernerSmartEmbeddableLib from './cerner-smart-embeddable-lib';
 
-CernerSmartEmbeddableLib.init();
-CernerSmartEmbeddableLib.listenForCustomFrameHeight();
+// CernerSmartEmbeddableLib.init();
+// CernerSmartEmbeddableLib.listenForCustomFrameHeight();
 
-window.CernerSmartEmbeddableLib = window.CernerSmartEmbeddableLib || {};
-window.CernerSmartEmbeddableLib.calcFrameHeight = CernerSmartEmbeddableLib.calcFrameHeight;
-window.CernerSmartEmbeddableLib.setFrameHeight = CernerSmartEmbeddableLib.setFrameHeight;
+// window.CernerSmartEmbeddableLib = window.CernerSmartEmbeddableLib || {};
+// window.CernerSmartEmbeddableLib.calcFrameHeight = CernerSmartEmbeddableLib.calcFrameHeight;
+// window.CernerSmartEmbeddableLib.setFrameHeight = CernerSmartEmbeddableLib.setFrameHeight;
